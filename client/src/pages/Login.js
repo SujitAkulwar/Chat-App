b0945088c216edabef3b4016e84649a85c8737e5
sujit akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2023-02-13 15:02:02 +0530

    login register

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,38 @@
-import React from 'react'
+// import React,{useState} from 'react'
+// import { ReactDOM } from 'react';
 import Nav from '../components/Nav'
+import './Login.css';
+
 const Login = () => {
+
+  // const [errorMessages, setErrorMessages] = useState({});
+
+  const handleSubmit = (event) => {
+    console.log("ok");
+  };
+  
+  const loginform = (
+    <form onSubmit={handleSubmit}>
+      <label> Email : </label><input type={Text} className="name" name='name'></input>
+      <label> Password : </label><input type={Text} className="name" name='pass'></input>
+      <input type={'submit'} className="submit"></input>
+    </form>
+  );
+
+  
+  
+  
   return (
     <>
       <Nav/>
-      <div>Login</div>
+      <div>
+        <div className='c'>
+          Login
+        </div>
+        <div className='d'> 
+          {loginform}
+        </div>
+      </div> 
     </>
   );
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,39 @@
-import React from 'react'
+// import React,{useState} from 'react'
+// import { ReactDOM } from 'react';
 import Nav from '../components/Nav'
+import './Register.css';
 
 const Register = () => {
-    return (
-    <>
-        <Nav/>
-        <div>Register</div>
-    </>  
-    
-  )
-}
 
-export default Register
+  // const [errorMessages, setErrorMessages] = useState({});
+
+  const handleSubmit = (event) => {
+    console.log("ok");
+  };
+  
+  const Registerform = (
+    <form onSubmit={handleSubmit}>
+      <label> Email : </label><input type={Text} className="name" name='name'></input>
+      <label> Password : </label><input type={Text} className="name" name='pass'></input>
+      <input type={'submit'} className="submit"></input>
+    </form>
+  );
+
+  
+  
+  
+  return (
+    <>
+      <Nav/>
+      <div>
+        <div className='c'>
+          Register
+        </div>
+        <div className='d'> 
+          {Registerform}
+        </div>
+      </div> 
+    </>
+  );
+}
+export default Register;
